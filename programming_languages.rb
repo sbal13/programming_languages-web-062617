a0e70434246e270973eb286cdafc984af34228e3
Steven Balasta <sbal13@gmail.com>
2017-06-23 17:54:53 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  final = Hash.new
+  languages.each do |style, langs|
+    langs.each do |language, attributes|
+      final[language] ||= attributes
+      final[language][:style].nil? ? final[language][:style] = [style] : final[language[:style]] << style
+    end
+  end
 end
